d52bdce56b52339c8eb1448387c61507e1ceeadc
Shi Rao <github@homoantiquum.com>
2024-01-29 13:00:17 -0500

    netrw option

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,7 @@ vim.g.mapleader = " "
 vim.g.netrw_banner = 0
 vim.g.netrw_browse_split = 4
 vim.g.netrw_liststyle = 2
+vim.g.netrw_list_hide = [[^\(ntuser\|NTUSER\)\..*]]
 vim.g.netrw_winsize = -20
 
 -- key map to emulate <c-[> in vim terminal
