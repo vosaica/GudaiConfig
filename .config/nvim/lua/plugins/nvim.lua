ec220a8fceb0c8b393fca4088c4f3c934ddcc7fd
Shi Rao <github@homoantiquum.com>
2024-03-02 00:59:50 -0500

    changed completion plugin

--- .config/nvim/lua/plugins/nvim.lua
+++ .config/nvim/lua/plugins/nvim.lua
@@ -1,5 +1,37 @@
 return {
 	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+		},
+		opts = function()
+			local cmp = require("cmp")
+			return {
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<c-b>"] = cmp.mapping.scroll_docs(-4),
+					["<c-f>"] = cmp.mapping.scroll_docs(4),
+					["<c-space>"] = cmp.mapping.complete(),
+					["<c-e>"] = cmp.mapping.abort(),
+					["<cr>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "vsnip" } }, { { name = "buffer" } }),
+			}
+		end,
+	},
+	{
 		"echasnovski/mini.completion",
 		opts = {
 			window = {
@@ -88,6 +120,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
 			"williamboman/mason.nvim",
+			"hrsh7th/nvim-cmp",
 		},
 		opts = {
 			ensure_installed = {
@@ -98,7 +131,10 @@ return {
 			},
 			handlers = {
 				function(server_name)
-					require("lspconfig")[server_name].setup({})
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
 				end,
 			},
 		},
